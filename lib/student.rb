38d0f33983453616633aacc9806a9fdd38a36732
M Banks <montensia1@outlook.com>
2019-06-19 03:42:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(id = nil, name, grade)
+  def self.new(id = nil, name, grade)
     @name = name
     @grade = grade
     @id = id
